e6dbf2ca938f62289d8ba9ca0ff6c771f3591ed0
Koitharu <nvasya95@gmail.com>
2026-09-14 12:34:42 +0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "org.koitharu.album"
         minSdk = 24
         targetSdk = 37
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
     }
 
     buildTypes {
